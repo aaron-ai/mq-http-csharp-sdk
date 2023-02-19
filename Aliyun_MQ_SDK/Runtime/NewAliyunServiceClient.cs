8a2c03f7f85aeac4180ee3f75ed6fff1edcb44b3
Aaron Ai <yangkun.ayk@gmail.com>
2023-02-19 20:34:18 +0800

    Replace HttpWebRequest by HttpClient

--- Aliyun_MQ_SDK/Runtime/NewAliyunServiceClient.cs
+++ Aliyun_MQ_SDK/Runtime/NewAliyunServiceClient.cs
@@ -1,14 +1,71 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
 using Aliyun.MQ.Runtime.Internal;
 using Aliyun.MQ.Runtime.Internal.Auth;
 using Aliyun.MQ.Runtime.Internal.Transform;
+using Aliyun.MQ.Runtime.Pipeline;
+using Aliyun.MQ.Runtime.Pipeline.ErrorHandler;
+using Aliyun.MQ.Runtime.Pipeline.Handlers;
+using Aliyun.MQ.Runtime.Pipeline.HttpHandler;
+using Aliyun.MQ.Runtime.Pipeline.RetryHandler;
 
 namespace Aliyun.MQ.Runtime
 {
     public abstract class NewAliyunServiceClient : IDisposable
     {
+        private bool _disposed;
+
+        protected RuntimePipeline RuntimePipeline { get; set; }
+        protected ServiceCredentials Credentials { get; private set; }
+        internal ClientConfig Config { get; private set; }
+
         internal NewAliyunServiceClient(ServiceCredentials credentials, ClientConfig config)
         {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.DefaultConnectionLimit = config.ConnectionLimit;
+            ServicePointManager.MaxServicePointIdleTime = config.MaxIdleTime;
+
+            this.Config = config;
+            this.Credentials = credentials;
+            Signer = CreateSigner();
+
+            Initialize();
+
+            BuildRuntimePipeline();
+        }
+
+        protected IServiceSigner Signer { get; private set; }
+
+        protected virtual void Initialize()
+        {
+        }
+
+        protected virtual void CustomizeRuntimePipeline(RuntimePipeline pipeline)
+        {
+        }
+
+        private void BuildRuntimePipeline()
+        {
+            // Replace HttpWebRequestFactory by HttpRequestMessageFactory
+            var httpRequestFactory = new HttpRequestMessageFactory(Config);
+            var httpHandler = new HttpHandler<HttpContent>(httpRequestFactory, this);
+
+            this.RuntimePipeline = new RuntimePipeline(new List<IPipelineHandler>
+                {
+                    httpHandler,
+                    new Unmarshaller(),
+                    new ErrorHandler(),
+                    new Signer(),
+                    new CredentialsRetriever(this.Credentials),
+                    new RetryHandler(new DefaultRetryPolicy(this.Config.MaxErrorRetry)),
+                    new Marshaller()
+                }
+            );
+
+            CustomizeRuntimePipeline(this.RuntimePipeline);
         }
 
         internal NewAliyunServiceClient(string accessKeyId, string secretAccessKey, ClientConfig config,
@@ -20,7 +77,8 @@ namespace Aliyun.MQ.Runtime
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
 
         protected abstract IServiceSigner CreateSigner();
@@ -30,20 +88,43 @@ namespace Aliyun.MQ.Runtime
             return;
         }
 
-        protected TResponse Invoke<TRequest, TResponse>(TRequest request,
+        private void ThrowIfDisposed()
+        {
+            if (this._disposed)
+                throw new ObjectDisposedException(GetType().FullName);
+        }
+
+        internal TResponse Invoke<TRequest, TResponse>(TRequest request,
             IMarshaller<IRequest, WebServiceRequest> marshaller, ResponseUnmarshaller unmarshaller)
             where TRequest : WebServiceRequest
             where TResponse : WebServiceResponse
         {
-            return null;
+            ThrowIfDisposed();
+
+            var executionContext = new ExecutionContext(
+                new RequestContext()
+                {
+                    ClientConfig = this.Config,
+                    Marshaller = marshaller,
+                    OriginalRequest = request,
+                    Signer = Signer,
+                    Unmarshaller = unmarshaller,
+                    IsAsync = false
+                },
+                new ResponseContext()
+            );
+
+            var response = (TResponse)this.RuntimePipeline.InvokeSync(executionContext).Response;
+            return response;
         }
 
-        protected void Invoke<TRequest, TResponse>(TRequest request,
+        protected System.Threading.Tasks.Task<TResponse> InvokeAsync<TRequest, TResponse>(
+            TRequest request,
             IMarshaller<IRequest, WebServiceRequest> marshaller,
             ResponseUnmarshaller unmarshaller,
             System.Threading.CancellationToken cancellationToken)
             where TRequest : WebServiceRequest
-            where TResponse : WebServiceResponse
+            where TResponse : WebServiceResponse, new()
         {
             return null;
         }
